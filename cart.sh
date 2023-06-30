c8414db0b8367872969c050b76d35961b5815b8c
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-06-30 10:28:34 +0530

    cart script

--- cart.sh
+++ cart.sh
@@ -1,14 +1,33 @@
+echo -e "\e[34m>>>>>>>>>configuring NodeJS<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[34m>>>>>>>>>Install NodeJS<<<<<<<<<\e[0m"
 yum install nodejs -y
+
+echo -e "\e[34m>>>>>>>>>Add Application user<<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[34m>>>>>>>>>Create Application Directory<<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
-curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
+
+echo -e "\e[34m>>>>>>>>>Download App Content <<<<<<<<<\e[0m"
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
+
+echo -e "\e[34m>>>>>>>>>Unzip App Content<<<<<<<<<\e[0m"
 unzip /tmp/cart.zip
 cd /app
+
+echo -e "\e[34m>>>>>>>>>Install NodeJS Dependencies<<<<<<<<<\e[0m"
 npm install
-cp  cart.service  /etc/systemd/system/cart.service
+
+echo -e "\e[34m>>>>>>>>>Copy Cart SystemD file<<<<<<<<<\e[0m"
+cp  /home/centos/roboshop-shell/cart.service  /etc/systemd/system/cart.service
+
+echo -e "\e[34m>>>>>>>>>Start Cart service<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable cart
-systemctl start cart
+systemctl restart cart
 
