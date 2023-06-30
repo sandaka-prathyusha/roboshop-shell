a6666053a9bc0d4405a5e198259b4bc8d5fe2390
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-06-30 09:33:24 +0530

    user Application

--- user.sh
+++ user.sh
@@ -1,17 +1,43 @@
+echo -e "\e[34m>>>>>>>>>Dowloading NodeJS Repos <<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+
+echo -e "\e[34m>>>>>>>>> Installing NodeJS <<<<<<<<<\e[0m"
 yum install nodejs -y
+
+echo -e "\e[34m>>>>>>>>>Adding Application User<<<<<<<<<\e[0m"
 useradd roboshop
+
+rm -rf  /app
 mkdir /app
+
+echo -e "\e[34m>>>>>>>>>Download Application Content<<<<<<<<<\e[0m"
 curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
+
+echo -e "\e[34m>>>>>>>>Unzip App Content<<<<<<<<<<<\e[0m"
 unzip /tmp/user.zip
 cd /app
+
+echo -e "\e[34m>>>>>>>>>Install NodeJS Dependencies<<<<<<<<<\e[0m"
 npm install
+
+
+echo -e "\e[34m>>>>>>>>>Copy user SystemD file<<<<<<<<<\e[0m"
 cp user.service  /etc/systemd/system/user.service
 systemctl daemon-reload
+
+
+echo -e "\e[34m>>>>>>>>>Starting user Service<<<<<<<<<\e[0m"
 systemctl enable user
-systemctl start user
+systemctl restart user
+
+echo -e "\e[34m>>>>>>>>>Copy MongoDB repos<<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[34m>>>>>>>>>Install Mongo Client<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
+
+echo -e "\e[34m>>>>>>Load Schema<<<<<<<<<\e[0m"
 mongo --host mongodb-dev.nandu18.online</app/schema/catalogue.js
 
