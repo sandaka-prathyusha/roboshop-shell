87d3c7a3f4f31a842843b67613ef280ed3279eab
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-06-30 10:51:08 +0530

    mysql script

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,20 @@
+echo -e "\e[36m>>>>>>>>>Disabling Default MYSQL <<<<<<<<<\e[0m"
 dnf module disable mysql -y
+
+
+
+echo -e "\e[36m>>>>>>>>>COPYING MYSQL Repos<<<<<<<<<\e[0m"
 cp mysql.repo  /etc/yum.repos.d/mysql.repo
+
+
+echo -e "\e[36m>>>>>>>>>Install MYSQL <<<<<<<<<\e[0m"
 yum install mysql-community-server -y
+
+
+echo -e "\e[36m>>>>>>>>>Starting mysql<<<<<<<<<\e[0m"
 systemctl enable mysqld
 systemctl start mysqld
+
+
+echo -e "\e[36m>>>>>>>>>Changing root password<<<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
