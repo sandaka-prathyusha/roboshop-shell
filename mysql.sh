97672d7cc9166c6eae91a720349e3c64a44ca610
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-07-01 13:11:06 +0530

    righting the scripts

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+${mysql_root_password}
 
 echo -e "\e[36m>>>>>>>>>Disabling Default mysql<<<<<<<<<\e[0m"
 dnf module disable mysql -y
@@ -16,4 +17,4 @@ systemctl enable mysqld
 systemctl restart mysqld
 
 echo -e "\e[36m>>>>>>>>>Changing root password<<<<<<<<<\e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass ${mysql_root_password}
