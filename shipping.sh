34989a42ba88c5ed67720433bf18327a9750d413
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-07-01 18:23:11 +0530

    shipping

--- shipping.sh
+++ shipping.sh
@@ -1,7 +1,8 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-${mysql_root_password}
+mysql_root_password=$1
+
 echo -e "\e[36m>>>>>>>>>Java Installation <<<<<<<<<\e[0m"
 yum install maven -y
 
