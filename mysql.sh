03e8b80b67e27519ab3ae29a10f5a536c10d49b4
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-07-01 10:00:39 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -2,13 +2,13 @@ script=$(realpath "$0")
 script_path=$(dirname "script ")
 source ${script_path}/common.sh
 
-echo -e "\e[36m>>>>>>>>>Disabling Default MYSQL <<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>Disabling Default mysql<<<<<<<<<\e[0m"
 dnf module disable mysql -y
 
 echo -e "\e[36m>>>>>>>>>COPYING MYSQL Repos<<<<<<<<<\e[0m"
 cp mysql.repo  /etc/yum.repos.d/mysql.repo
 
-echo -e "\e[36m>>>>>>>>>Install MYSQL <<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>Install mysql<<<<<<<<<\e[0m"
 yum install mysql-community-server -y
 
 echo -e "\e[36m>>>>>>>>>Starting mysql<<<<<<<<<\e[0m"
