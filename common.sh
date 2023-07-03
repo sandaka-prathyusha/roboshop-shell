f6660800045cb33563419b57d61298bb802bcf05
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-07-03 14:54:13 +0530

    mistake

--- common.sh
+++ common.sh
@@ -1,7 +1,6 @@
 app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
-source ${script_path}/common.sh
 
 func_nodejs() {
 echo -e "\e[34m>>>>>>>>>configuring NodeJS<<<<<<<<<\e[0m"
