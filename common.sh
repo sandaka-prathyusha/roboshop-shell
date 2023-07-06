47b801f37a93147bf778c61d7d4eeb815949581b
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-07-06 12:49:37 +0530

    common.sh

--- common.sh
+++ common.sh
@@ -7,52 +7,52 @@
  }
 
  schema_setup(){
-   if ["$schema_setup" == "mongo"]; then
-   print_head "Copy MongoDB repos"
-   cp $script_path/mongo.repo /etc/yum.repos.d/mongo.repo
-
-   print_head "Install Mongo Client"
-   yum install mongodb-org-shell -y
-
-   print_head "Load schema"
-   mongo --host mongodb-dev.nandu18.online</app/schema/${component}.js
+   if [ "$schema_setup" == "mongo"]; then
+     print_head "INSTALL MONGODB REPO"
+     cp ${script_path}/mongo.repo  /etc/yum.repos.d/mongo.repo
+     print_head "INSTALL MONGODB CLIENT"
+    yum install mongodb-org-shell -y
+     print_head "LOAD SCHEMA"
+     mongo --host mongodb-dev.nandu18.online  </app/schema/${component}.js
    fi
  }
 
 
  func_nodejs() {
 
- print_head "configuring NodeJS"
- curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
- print_head "Install NodeJS"
- yum install nodejs -y
+   print_head "configuring NodeJS"
+   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
- print_head "Add Application user"
- useradd ${app_user}
+   print_head "Install NodeJS"
+    yum install nodejs -y
 
- print_head "Create Application Directory"
- rm -rf /app
- mkdir /app
+   print_head "Add Application user"
+    useradd ${app_user}
+
+   print_head "Create Application Directory"
+    rm -rf /app
+    mkdir /app
 
 
- print_head "Download App Content"
- curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
- cd /app
+   print_head "Download App Content"
+    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+    cd /app
 
- print_head "Unzip App Content"
- unzip /tmp/${component}.zip
- cd /app
+   print_head "Unzip App Content"
+   unzip /tmp/${component}.zip
+   cd /app
 
- print_head "Install NodeJS Dependencies"
- npm install
+   print_head "Install NodeJS Dependencies"
+   npm install
 
- print_head "Copy Cart SystemD file"
- cp  ${script_path}/${component}.service  /etc/systemd/system/${component}.service
+   print_head "Copy SystemD file"
+   cp  ${script_path}/${component}.service  /etc/systemd/system/${component}.service
 
- print_head "Start Cart service"
- systemctl daemon-reload
- systemctl enable ${component}
- systemctl restart ${component}
- schema_setup
+   print_head "Start Cart service"
+   systemctl daemon-reload
+   systemctl enable ${component}
+   systemctl restart ${component}
+   schema_setup
  }
+
