bcadfe88fd790ad4c36d818e58f338d1a2e4f800
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-07-06 17:55:13 +0530

    dry

--- common.sh
+++ common.sh
@@ -2,57 +2,90 @@
  script=$(realpath "$0")
  script_path=$(dirname "$script")
 
- print_head(){
+ func_print_head(){
   echo -e "\e[33m>>>>>>>>> $1 <<<<<<<<<\e[0m"
  }
 
- schema_setup(){
+ func_schema_setup() {
    if [ "$schema_setup" == "mongo"]; then
-     print_head "INSTALL MONGODB REPO"
+     func_print_head "INSTALL MONGODB REPO"
      cp ${script_path}/mongo.repo  /etc/yum.repos.d/mongo.repo
-     print_head "INSTALL MONGODB CLIENT"
-    yum install mongodb-org-shell -y
-     print_head "LOAD SCHEMA"
+
+     func_print_head "INSTALL MONGODB CLIENT"
+     yum install mongodb-org-shell -y
+     func_print_head "LOAD SCHEMA"
      mongo --host mongodb-dev.nandu18.online  </app/schema/${component}.js
    fi
+   if [ "$schema_setup" == "mysql"]; then
+
+     func_print_head  "INSTALL MYSQL Client"
+     yum install mysql -y
+
+     func_print_head  "LOAD SCHEMA"
+     mysql -h  mysql-dev.nandu18.online -uroot -p${mysql_root_passwrd} <  /app/schema/shipping.sql
+   fi
+
  }
 
+
+ func_app_prereq() {
+
+     func_print_head  "Adding Application User"
+     useradd ${app_user}
+
+     func_print_head  "Create Application Directory"
+     rm -rf /app
+     mkdir /app
+
+     func_print_head "Downloading Application Content"
+     curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+     cd /app
+
+     func_print_head "Unzip Application content"
+     unzip /tmp/${component}.zip
+    }
+
+func_systemd_setup() {
+     func_print_head "Setup SystemD file"
+     cp  ${script_path}/${component}.service  /etc/systemd/system/${component}.service
+
+     func_print_head "Start ${component} service"
+     systemctl daemon-reload
+     systemctl enable ${component}
+     systemctl restart ${component}
+    }
 
  func_nodejs() {
 
+    func_print_head "configuring NodeJS"
+    curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-   print_head "configuring NodeJS"
-   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-
-   print_head "Install NodeJS"
+    func_print_head "Install NodeJS"
     yum install nodejs -y
 
-   print_head "Add Application user"
-    useradd ${app_user}
+    func_app_prereq
 
-   print_head "Create Application Directory"
-    rm -rf /app
-    mkdir /app
+    func_print_head "Install NodeJS Dependencies"
+    npm install
 
+    func_schema_setup
+    func_systemd_setup
+   }
 
-   print_head "Download App Content"
-    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-    cd /app
+func_java() {
 
-   print_head "Unzip App Content"
-   unzip /tmp/${component}.zip
-   cd /app
+  func_print_head "Install Maven"
+  yum install maven -y
 
-   print_head "Install NodeJS Dependencies"
-   npm install
+  func_app_prereq
 
-   print_head "Copy SystemD file"
-   cp  ${script_path}/${component}.service  /etc/systemd/system/${component}.service
+  func_print_head "Downloading Maven Dependencies"
+  mvn clean package
 
-   print_head "Start Cart service"
-   systemctl daemon-reload
-   systemctl enable ${component}
-   systemctl restart ${component}
-   schema_setup
- }
+  func_print_head  "Move the file"
+  mv target/${component}-1.0.jar   ${component}.jar
 
+  func_schema_setup
+  func_systemd_setup
+}
+  }
--- shipping.sh
+++ shipping.sh
@@ -8,41 +8,7 @@ if [ -z "$mysql_root_password"]; then
    exit
 fi
 
-echo -e "\e[36m>>>>>>>>>Java Installation <<<<<<<<<\e[0m"
-yum install maven -y
+component="shipping"
+schema_setup=mysql
+func_java
 
-echo -e "\e[36m>>>>>>>>>Adding Application User <<<<<<<<<\e[0m"
-useradd ${app_user}
-
-echo -e "\e[36m>>>>>>>>>Create Application Directory <<<<<<<<<\e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[36m>>>>>>>Downloading Application Content <<<<<<<<<\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-
-echo -e "\e[36m>>>>>>>>>Unzip Application content<<<<<<<<<\e[0m"
-unzip /tmp/shipping.zip
-cd /app
-
-echo -e "\e[36m>>>>>>>>> Downloading Dependencies <<<<<<<<<\e[0m"
-mvn clean package
-
-echo -e "\e[36m>>>>>>>>>move the file<<<<<<<<<\e[0m"
-mv target/shipping-1.0.jar  shipping.jar
-
-echo -e "\e[36m>>>>>>>>>Coping systemd service file<<<<<<<<<\e[0m"
-cp ${script_path}/shipping.service  /etc/systemd/system/shipping.service
-
-echo -e "\e[36m>>>>>>>>>START SHIPPING<<<<<<<<<\e[0m"
-systemctl daemon-reload
-
-echo -e "\e[36m>>>>>>>>>INSTALL MYSQL <<<<<<<<<\e[0m"
-yum install mysql -y
-
-echo -e "\e[36m>>>>>>>>>LOAD SCHEMA <<<<<<<<<\e[0m"
-mysql -h  mysql-dev.nandu18.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
-
-echo -e "\e[36m>>>>>>>>>RESTART SHIPPING <<<<<<<<<\e[0m"
-systemctl restart shipping
