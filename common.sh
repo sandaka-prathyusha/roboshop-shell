ec27a8d6accb4f9866c3c0db1789af17f41ebe01
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-07-06 21:06:05 +0530

    append redirection

--- common.sh
+++ common.sh
@@ -1,6 +1,8 @@
  app_user=roboshop
  script=$(realpath "$0")
  script_path=$(dirname "$script")
+ log_file=/tmp/roboshop.log
+
 
  func_print_head(){
   echo -e "\e[33m>>>>>>>>> $1 <<<<<<<<<\e[0m"
@@ -19,11 +21,11 @@
  func_schema_setup() {
    if [ "$schema_setup" == "mongo"]; then
      func_print_head "INSTALL MONGODB REPO"
-     cp ${script_path}/mongo.repo  /etc/yum.repos.d/mongo.repo
+     cp ${script_path}/mongo.repo  /etc/yum.repos.d/mongo.repo &>>$log_file
      func_stat_check $?
 
      func_print_head "INSTALL MONGODB CLIENT"
-     yum install mongodb-org-shell -y
+     yum install mongodb-org-shell -y &>>$log_file
      func_stat_check $?
      func_print_head "LOAD SCHEMA"
      mongo --host mongodb-dev.nandu18.online  </app/schema/${component}.js
@@ -32,7 +34,7 @@
    if [ "$schema_setup" == "mysql"]; then
 
      func_print_head  "INSTALL MYSQL Client"
-     yum install mysql -y
+     yum install mysql -y &>>$log_file
      func_stat_check $?
 
      func_print_head  "LOAD SCHEMA"
@@ -46,12 +48,12 @@
  func_app_prereq() {
 
      func_print_head  "Adding Application User"
-     useradd ${app_user} >/tmp/roboshop.log
+     useradd ${app_user} &>>$log_file
      func_stat_check $?
 
      func_print_head  "Create Application Directory"
-     rm -rf /app
-     mkdir /app
+     rm -rf /app &>>$log_file
+     mkdir /app &>>$log_file
      func_stat_check $?
 
      func_print_head "Downloading Application Content"
@@ -60,19 +62,19 @@
      cd /app
 
      func_print_head "Unzip Application content"
-     unzip /tmp/${component}.zip
+     unzip /tmp/${component}.zip &>>$log_file
      func_stat_check $?
     }
 
 func_systemd_setup() {
      func_print_head "Setup SystemD file"
-     cp  ${script_path}/${component}.service  /etc/systemd/system/${component}.service
+     cp  ${script_path}/${component}.service  /etc/systemd/system/${component}.service &>>$log_file
      func_stat_check $?
 
      func_print_head "Start ${component} service"
-     systemctl daemon-reload
-     systemctl enable ${component}
-     systemctl restart ${component}
+     systemctl daemon-reload &>>$log_file
+     systemctl enable ${component} &>>$log_file
+     systemctl restart ${component} &>>$log_file
      func_stat_check $?
     }
 
@@ -81,17 +83,17 @@ func_systemd_setup() {
  func_nodejs() {
 
     func_print_head "configuring NodeJS"
-    curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+    curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
     func_stat_check $?
 
     func_print_head "Install NodeJS"
-    yum install nodejs -y
+    yum install nodejs -y &>>$log_file
     func_stat_check $?
 
     func_app_prereq
 
     func_print_head "Install NodeJS Dependencies"
-    npm install
+    npm install &>>$log_file
     func_stat_check $?
 
     func_schema_setup
@@ -101,19 +103,19 @@ func_systemd_setup() {
 func_java() {
 
   func_print_head "Install Maven"
-  yum install maven -y >/tmp/roboshop.log
+  yum install maven -y &>>$log_file
   func_stat_check $?
   func_stat_check $?
 
   func_app_prereq
 
   func_print_head "Downloading Maven Dependencies"
-  mvn clean package
+  mvn clean package &>>$log_file
 
   func_stat_check $?
 
   func_print_head  "Move the file"
-  mv target/${component}-1.0.jar   ${component}.jar
+  mv target/${component}-1.0.jar   ${component}.jar &>>$log_file
 
   func_schema_setup
   func_systemd_setup
