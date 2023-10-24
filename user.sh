0cf2d8b0fbc60191df24fac76f6b32d9730f8baa
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-10-24 17:16:09 +0530

    scriptpath deletion

--- user.sh
+++ user.sh
@@ -1,7 +1,8 @@
-script=$(realpath "$0")
-script_path=$(dirname "$script ")
+#script=$(realpath "$0")
+#script_path=$(dirname "$script ")
 
-source ${script_path}/common.sh
+#source ${script_path}/common.sh
+source common.sh
 
 component=user
 
